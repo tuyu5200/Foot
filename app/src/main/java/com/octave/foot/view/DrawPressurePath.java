ac0692b25a8a63b5651458cf1df4e1e10447ede0
HS <71546563@qq.com>
2017-01-09 23:28:24 +0800

    hahaha

--- app/src/main/java/com/octave/foot/view/DrawPressurePath.java
+++ app/src/main/java/com/octave/foot/view/DrawPressurePath.java
@@ -1,6 +1,7 @@
 package com.octave.foot.view;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
@@ -31,18 +32,56 @@ public class DrawPressurePath extends View {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         return super.onTouchEvent(event);
-        
+
+    }
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        super.onDraw(canvas);
+        //通过画布绘制多点形成的图形
+        canvas.drawPath(mPath, mGesturePaint);
     }
 
-    public DrawPressurePath(Context context, AttributeSet attrs) {
-        super(context, attrs);
+    //手指点下屏幕时调用
+    private void touchDown(MotionEvent event)
+    {
+        //mPath.rewind();
+        //重置绘制路线，即隐藏之前绘制的轨迹
+        mPath.reset();
+        float x = event.getX();
+        float y = event.getY();
+
+        mX = x;
+        mY = y;
+        //mPath绘制的绘制起点
+        mPath.moveTo(x, y);
     }
 
-    public DrawPressurePath(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
+    //手指在屏幕上滑动时调用
+    private void touchMove(MotionEvent event)
+    {
+        final float x = event.getX();
+        final float y = event.getY();
 
-    public DrawPressurePath(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
+        final float previousX = mX;
+        final float previousY = mY;
+
+        final float dx = Math.abs(x - previousX);
+        final float dy = Math.abs(y - previousY);
+
+        //两点之间的距离大于等于3时，生成贝塞尔绘制曲线
+        if (dx >= 3 || dy >= 3)
+        {
+            //设置贝塞尔曲线的操作点为起点和终点的一半
+            float cX = (x + previousX) / 2;
+            float cY = (y + previousY) / 2;
+
+            //二次贝塞尔，实现平滑曲线；previousX, previousY为操作点，cX, cY为终点
+            mPath.quadTo(previousX, previousY, cX, cY);
+
+            //第二次执行时，第一次结束调用的坐标值将作为第二次调用的初始坐标值
+            mX = x;
+            mY = y;
+        }
     }
 }
